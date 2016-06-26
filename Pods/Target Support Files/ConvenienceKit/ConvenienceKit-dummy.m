b4d049e2fd0c160cc0dca5de183d5865a9679105
Daniel Haaser <thedapperdan@gmail.com>
2016-06-26 19:24:31 -0400

    check cocoapods into source control, make sure part1-solution builds

---
+++ Pods/Target Support Files/ConvenienceKit/ConvenienceKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ConvenienceKit : NSObject
+@end
+@implementation PodsDummy_ConvenienceKit
+@end
---
+++ Pods/Target Support Files/ConvenienceKit/ConvenienceKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ConvenienceKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char ConvenienceKitVersionString[];
+
